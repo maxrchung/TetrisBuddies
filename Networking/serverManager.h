188ab2c3f0a1adaa5c81c75ac82c30edf343dac2
hectoram <hectoram@uci.edu>
2014-11-03 21:48:36 -0800

    Server can now loginUser and registerUser

--- Networking/serverManager.h
+++ Networking/serverManager.h
@@ -2,10 +2,11 @@
 #define SERVER_MANAGER_H
 
 #pragma comment(lib, "sfml-network.lib")
-
 #include <iostream>
-
+#include <list>
 #include <SFML/Network.hpp>
+#include "DatabaseManager.h"
+#include "UserInfo.h"
 
 class serverManager
 {
@@ -20,6 +21,21 @@ public:
 	void sendPlayer(std::string name, int playerID, int rank);
 
 	void terminateConnection();
+
+	bool loginUser(std::string username, std::string password);
+
+	void registerUser(std::string username, std::string password);
+
+
+
+private:
+	sf::TcpListener listener;
+	sf::TcpSocket server;
+
+	std::list<sf::TcpSocket*> clients;
+	sf::SocketSelector selector;
+
+	std::list<UserInfo*> usersConnected;
 
 
 };
