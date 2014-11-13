e533971d665d773d16cd520fccf83a142cee0ce2
Anthony <soa2@uci.edu>
2014-11-13 15:12:14 -0800

    Gameplay added

--- BlockBuddies Main/BlockBuddies/GameScreen.hpp
+++ BlockBuddies Main/BlockBuddies/GameScreen.hpp
@@ -2,7 +2,9 @@
 #define GAMESCREEN_HPP
 
 #include "Screen.hpp"
-
+#include "GraphicsManager.hpp"
+#include "BlockHandler.hpp"
+#include "CursorHandler.hpp"
 // This is the Screen for the actual game itself
 // We will most likely need to make separate games for separate game types,
 // i.e. ranked, casual, and solo play, but for now we have this single GameScreen
@@ -13,6 +15,13 @@ public:
 	GameScreen();
 	void update();
 	void draw();
+
+private:
+	BlockHandler *bh;
+	CursorHandler *ch;
+	bool pressed, pressed2;
+	sf::RectangleShape rec;
+
 };
 
 #endif//GAMESCREEN_HPP
