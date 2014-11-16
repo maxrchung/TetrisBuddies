ce0ee183529c4348692ec0e30591fb9c1edd35ae
Tommy Tran <Tommyht1@uci.edu>
2014-11-15 17:04:07 -0800

    More setup

--- BlockBuddies Main/BlockBuddiesServer/Messager.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Messager
-{
-public:
-	Messager();
-	~Messager();
-};
-
---
+++ BlockBuddies Main/BlockBuddiesServer/Messenger.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Message.h"
+#include "MatchMakingHandler.h"
+#include "NetworkManager.h"
+class Messenger
+{
+public:
+	Messenger();
+	~Messenger();
+};
+
