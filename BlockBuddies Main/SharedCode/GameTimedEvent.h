a9b889088ac212fcfe0a60e82b9b474584677fbf
DJTedMoney <afranowi@uci.edu>
2015-01-22 15:50:34 -0800

    Added a .h that enums all the timed game events

---
+++ BlockBuddies Main/SharedCode/GameTimedEvent.h
@@ -0,0 +1,20 @@
+//this file is where all the events in the game that need some sort of timer are named.
+
+#ifndef GAMETIMEDEVENT_H
+#define GAMETIMEDEVENT_H
+
+enum GameEvent{
+	//0
+	BLOCKS_SWAPPING,
+	
+	//1
+	BLOCKS_CLEARING,
+
+	//2
+	SCROLL_PAUSE,
+
+	//3
+	BLOCKS_FALLING
+};
+
+#endif
