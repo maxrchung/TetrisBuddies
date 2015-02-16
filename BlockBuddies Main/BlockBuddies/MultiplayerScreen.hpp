c422ceb7678626e17346d78430a137a4ccd38fe0
TommyTran93 <TommyTran1993@gmail.com>
2015-02-15 16:35:09 -0800

    Multiplayer update

--- BlockBuddies Main/BlockBuddies/MultiplayerScreen.hpp
+++ BlockBuddies Main/BlockBuddies/MultiplayerScreen.hpp
@@ -5,21 +5,36 @@
 #include "GraphicsManager.hpp"
 #include "ScreenManager.hpp"
 #include "CursorHandler.hpp"
+#include "GameStateObject.hpp"
+#include "ClientManager.h"
+#include "UIElement.hpp"
+#include "TextBox.hpp"
+#include "SoundManager.h"
+#include "InputManager.hpp"
+
 class MultiplayerScreen : public Screen
 {
 public:
 	MultiplayerScreen();
 	void update();
 	void draw();
-    void reload();
+	void reload();
 private:
 	CursorHandler *ch;
-	sf::RectangleShape p1Blocks[20][16];
-	sf::RectangleShape p2Blocks[20][16];
+	sf::RectangleShape p1Blocks[GameStateObject::boardHeight][GameStateObject::boardWidth];
+	sf::RectangleShape p2Blocks[GameStateObject::boardHeight][GameStateObject::boardWidth];
 	sf::RectangleShape p1Outline, p2Outline;
+	GameStateObject p1GSO;
+	GameStateObject p2GSO;
+	bool pressed, pressed2;
 	bool blockSwitch;
+	void initGame();
+	bool reset;
+	int blockSizeX;
+	int blockSizeY;
+	void updateBlocks();
+	sf::Sound swapSound;
 	const int SCREENWIDTH = 400;
 	const int SCREENHEIGHT = 500;
 };
-
 #endif//MULTIPLAYERSCREEN_HPP
