807c62d127576ae9e7dc10f3d4ba30529c4a18ab
Hector Martinez <hectoram@uci.edu>
2015-03-05 16:09:19 -0800

    IP address now read in from file (Ip.txt)

--- BlockBuddies Main/BlockBuddies/LoginScreen.cpp
+++ BlockBuddies Main/BlockBuddies/LoginScreen.cpp
@@ -160,7 +160,11 @@ void LoginScreen::update()
         }
         if (!ClientManager::getInstance().isConnected)
         {
-			if (!ClientManager::getInstance().initConnection(sf::IpAddress("169.234.10.163"), 5000))
+			std::ifstream in("IP.txt");
+			//Read in IP to string.
+			std::string ip;
+			getline(in, ip);
+			if (!ClientManager::getInstance().initConnection(sf::IpAddress(ip), 5000))
 			{
                 ScreenManager::getInstance()->addScreen(Screens::NOTIFICATION, "Failed to connect with server. The server may be down, or you may not be connected to the Internet.");
 				if (sound.getStatus() != sound.Playing)
